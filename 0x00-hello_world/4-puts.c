12146e715c13ed1b58241c916bfb271560a59e15
oliviamungai <oliviamungai00@gmail.com>
2023-03-16 14:21:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- * Description: main - prints out a string to stdout
- * Return 0 if success
- */
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
