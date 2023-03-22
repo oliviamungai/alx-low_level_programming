739e4eb932565fe8b6c5d2c5446d9105aeec4c4d
oliviamungai <oliviamungai00@gmail.com>
2023-03-22 10:53:04 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
-   * main - the program starts here
-   *
-   * Return: Success(0)
-   */
+ * main - function to execute the block of code
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	char c[8] = "_putchar";
+	unsigned int i;
+
+	for (i = 0; i < sizeof(c); i++)
+	{
+		_putchar(c[i]);
+	}
+_putchar('\n');
+return (0);
 }
