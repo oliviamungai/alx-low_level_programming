ca7be831e4167f03d1a75824df890734e9e0a9ff
oliviamungai <oliviamungai00@gmail.com>
2023-03-28 10:43:33 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * rev_string - reverses a string
+  * @s: input point for function
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	long int a;
+	int c;
+
+	char revstr[9000];
+	int b = 0;
+
+	for (a = (strlen(s) - 1); a >= 0; a--)
+	{
+		revstr[b] = s[a];
+		b++;
+	}
+	for (c = 0; c <= (int)(strlen(revstr)); c++)
+	{
+		s[c] = revstr[c];
+	}
+}
